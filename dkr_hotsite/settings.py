e66645f3afe0f96e4bbf81bd414c1534b6840738
David <davidbarenco@gmail.com>
2018-06-02 21:09:22 -0300

    Make the basic directories for the templates and static files, along with it's configuration on the 'settings.py' file. Configured the 'urls.py' and 'views.py' files to link to the templates in the folder.

--- dkr_hotsite/settings.py
+++ dkr_hotsite/settings.py
@@ -2,6 +2,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -43,7 +45,7 @@ ROOT_URLCONF = 'dkr_hotsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,3 +109,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
