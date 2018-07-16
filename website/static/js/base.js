7480b677d550cab479982f8098e99e7fb96fce62
barenco <davidbarenco@gmail.com>
2018-07-16 09:58:47 -0300

    Make the pop-up alert for the child option in the event.

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -13,17 +13,14 @@ $("#id_payment_1").on("click", function(){
 // CHANGING THE PAYMENT SECTION DISPLAY ACCORDING TO THE EVENT CHOSED - ADULT OR KID
 $("#id_event_option_0").on("click", function(){
       $('#cielo_link').prop('href', 'https://cieloecommerce.cielo.com.br/TransactionalVNext/Checkout/Index/f0111d24-26ca-47a5-9f69-c77335892ff5')
-      $('.payment_form').css('display', 'block')
-      $('.payment').css('display', 'block')
+      $('.payment_form, .payment').css('display', 'block')
 });
 $("#id_event_option_1").on("click", function(){
       $('#cielo_link').prop('href', '')
-      $('.payment_form').css('display', 'block')
-      $('.payment').css('display', 'block')
+      $('.payment_form, .payment').css('display', 'block')
 });
 $("#id_event_option_2").on("click", function(){
-      $('.payment_form').css('display', 'none')
-      $('.payment').css('display', 'none')
+      $('.payment_form, .payment').css('display', 'none')
       $('.cancel_terms_area').css('margin-top', '50px')
 });
 
@@ -32,3 +29,12 @@ $("#id_event_option_2").on("click", function(){
 $("#id_payment_international_0").on("click", function () {
   window.open("https://www.paypal.com/");
 })
+
+
+$(document).ready(function(){
+
+      $('#id_event_option_1, #id_event_option_2').attr('data-toggle', 'modal')
+      $('#id_event_option_1, #id_event_option_2').attr('data-target', '#childModal')
+      $('h1').css('color', 'red')
+      // $('.payment').css('display', 'block')
+});
