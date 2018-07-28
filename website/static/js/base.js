261872681ac005b777d0069cf89949833df9e02a
barenco <davidbarenco@gmail.com>
2018-07-28 15:40:21 -0300

    Add some texts in english. Separate the 'accommodation' and 'transportation' tabs.

--- website/static/js/base.js
+++ website/static/js/base.js
@@ -33,3 +33,17 @@ $("#id_event_option_2").on("click", function(){
       $('.payment_form, .payment').css('display', 'none')
       $('.cancel_terms_area').css('margin-top', '50px')
 });
+
+// GOOGLE MAPS
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+// GO TO ACCOMODATION TAB
+$("#accomodation-link").on("click", function(){
+      $('#accomodation-tab').click()
+});
