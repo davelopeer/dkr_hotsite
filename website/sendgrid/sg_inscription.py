433e39d28b08721c46e2a26e482f12450ca719f4
barenco <davidbarenco@gmail.com>
2018-08-02 17:05:35 -0300

    Create the function to send the volunteer mail

--- website/sendgrid/sg_inscription.py
+++ website/sendgrid/sg_inscription.py
@@ -149,3 +149,83 @@ def sendMailHealthError(name,email,birthday,medical_agreement,coverage,phone,eme
         print(response.status_code)
         print(response.body)
         print(response.headers)
+
+def sendMailVolunteer(name, birthday, adress, adress_num, adress_comp,city, state, country, zip_code, email, phone, gender, initiations, initiations_lama, monastic_ordenation, observations, food_preferency, seat, physical_problems, joint_problems, health_problems, payment, payment_international, payment_info, event_option):
+        sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('DKR_SG_API_KEY'))
+        from_email = Email(email)
+        to_email = Email(os.environ.get('DEFAULT_TO_EMAIL'))
+        subject = "Formulário de inscrição - Drubchen de Khadro Sangdu"
+        sendgrid_content = Content("text/html", '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title></title><style media="screen">ul {font-size: 1.3em;}li {margin-bottom: 20px;}</style></head><body><h2>Dados Pessoais</h2><ul><li>Nome: ' + name +
+                                        '</li><li>Data de nascimento: ' + birthday +
+                                        '</li><li>Endereço: ' + adress +
+                                        '</li><li>Número: ' + adress_num +
+                                        '</li><li>Complemento: ' + adress_comp +
+                                        '</li><li>Cidade: ' + city +
+                                        '</li><li>Estado: ' + state +
+                                        '</li><li>País: ' + country +
+                                        '</li><li>CEP: ' + zip_code +
+                                        '</li><li>E-mail: ' + email +
+                                        '</li><li>Telefone/celular: ' + phone +
+                                        '</li><li>Gênero: ' + gender +
+                                        '</li><li>Iniciações: ' + initiations +
+                                        '</li><li>Professores que concederam as iniciações: ' + initiations_lama +
+                                        '</li><li>Ordenação monástica: ' + monastic_ordenation +
+                                        '</li><li>Observações gerais: ' + observations +
+                                        '</li><li>Preferência alimentar: ' + food_preferency +
+                                        '</li><li>Necessidade de cadeira: ' + seat +
+                                        '</li><li>Problema físico que impeça de carregar peso: ' + physical_problems +
+                                        '</li><li>Problema articular: ' + joint_problems +
+                                        '</li><li>Problema de saúde que cause limitação para atividades físicas ou para algum função: ' + health_problems +
+                                        '</li><li>Opção de evento: ' + event_option +
+                                        '</li><li>Forma de pagamento: ' + payment + payment_international +
+                                        '</li><li>Dados do pagamento: ' + payment_info +
+                                        '</li></ul></body></html>'
+                                    )
+
+
+        mail = Mail(from_email, subject, to_email, sendgrid_content)
+
+        response = sg.client.mail.send.post(request_body=mail.get())
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+
+def sendMailVolunteerError(name, birthday, adress, adress_num, adress_comp,city, state, country, zip_code, email, phone, gender, initiations, initiations_lama, monastic_ordenation, observations, food_preferency, seat, physical_problems, joint_problems, health_problems, payment, payment_international, payment_info, event_option):
+        sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('DKR_SG_API_KEY'))
+        from_email = Email(email)
+        to_email = Email(os.environ.get('DEFAULT_TO_EMAIL'))
+        subject = "Formulário de inscrição - Drubchen de Khadro Sangdu"
+        sendgrid_content = Content("text/html", '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title></title><style media="screen">ul {font-size: 1.3em;}li {margin-bottom: 20px;}</style></head><body><h2>Dados Pessoais</h2><ul><li>Nome: ' + name +
+                                        '</li><li>Data de nascimento: ' + birthday +
+                                        '</li><li>Endereço: ' + adress +
+                                        '</li><li>Número: ' + adress_num +
+                                        '</li><li>Complemento: ' + adress_comp +
+                                        '</li><li>Cidade: ' + city +
+                                        '</li><li>Estado: ' + state +
+                                        '</li><li>País: ' + country +
+                                        '</li><li>CEP: ' + zip_code +
+                                        '</li><li>E-mail: ' + email +
+                                        '</li><li>Telefone/celular: ' + phone +
+                                        '</li><li>Gênero: ' + gender +
+                                        '</li><li>Iniciações: ' + initiations +
+                                        '</li><li>Professores que concederam as iniciações: ' + initiations_lama +
+                                        '</li><li>Ordenação monástica: ' + monastic_ordenation +
+                                        '</li><li>Observações gerais: ' + observations +
+                                        '</li><li>Preferência alimentar: ' + food_preferency +
+                                        '</li><li>Necessidade de cadeira: ' + seat +
+                                        '</li><li>Problema físico que impeça de carregar peso: ' + physical_problems +
+                                        '</li><li>Problema articular: ' + joint_problems +
+                                        '</li><li>Problema de saúde que cause limitação para atividades físicas ou para algum função: ' + health_problems +
+                                        '</li><li>Opção de evento: ' + event_option +
+                                        '</li><li>Forma de pagamento: ' + payment + payment_international +
+                                        '</li><li>Dados do pagamento: ' + payment_info +
+                                        '</li></ul></body></html>'
+                                    )
+
+
+        mail = Mail(from_email, subject, to_email, sendgrid_content)
+
+        response = sg.client.mail.send.post(request_body=mail.get())
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
