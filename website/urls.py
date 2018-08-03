f150590097fcf1752eada84971782d263d6a3453
barenco <davidbarenco@gmail.com>
2018-08-03 15:54:24 -0300

    create a url path to the sponsor form

--- website/urls.py
+++ website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^guest-form/$', views.guest_form, name='guest-form'),
     url(r'^volunteer-form/$', views.volunteer_form, name='volunteer-form'),
     url(r'^cancellation-policy/$', views.cancellation_policy, name='cancellation-policy'),
+    url(r'^sponsor/$', views.sponsor, name='sponsor'),
 ]
