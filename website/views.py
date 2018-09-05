76cd6bda5dc6255268fa832427467db55621ba44
barenco <davidbarenco@gmail.com>
2018-09-05 15:23:28 -0300

    Create the function in the views to load the templete with the proper form

--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.template.loader import get_template
 from django.utils import translation
 from django.http import HttpResponseNotFound
-from website.forms import InscriptionForm, InscriptionFormEn, InscriptionFormEs, HealthForm, GuestForm, GuestFormEs, GuestFormEn, VolunteerForm, VolunteerFormEn, VolunteerFormEs, SponsorForm, SponsorFormEn, SponsoredForm, InscriptionSponsoredForm, InscriptionSponsoredFormEn, InscriptionSponsoredFormEs
+from website.forms import InscriptionForm, InscriptionFormEn, InscriptionFormEs, HealthForm, GuestForm, GuestFormEs, GuestFormEn, VolunteerForm, VolunteerFormEn, VolunteerFormEs, SponsorForm, SponsorFormEn, SponsoredForm, InscriptionSponsoredForm, InscriptionSponsoredFormEn, InscriptionSponsoredFormEs, SpecialGuestForm, SpecialGuestFormEn
 from website.sendgrid.sg_inscription import sendMail, sendMailHealth, sendMailVolunteer, sendMailError, sendMailHealthError, sendMailVolunteerError, sendMailSponsor, sendMailSponsored, sendMailSponsorError, sendMailSponsoredInscription, sendMailSponsoredInscriptionError
 
 def index(request):
@@ -792,6 +792,131 @@ def sponsored_form(request):
 
 
     return render(request, 'sponsored-form.html',  {
+        'form': form,
+        'user_language': user_language
+    })
+
+def special_guest_form(request):
+    user_language = translation.get_language_from_request(request, check_path=True)
+
+    if request.method == 'POST':
+        if user_language == 'pt-br':
+            form = SpecialGuestForm(request.POST)
+        elif user_language == 'es':
+            form = SpecialGuestForm(request.POST)
+        else:
+            form = SpecialGuestForm(request.POST)
+
+        # if (form.fields['payment']) == 'Depósito bancário':
+        #     form.fields['deposit_day'].required = True
+        name = request.POST.get('name', '')
+        birthday = request.POST.get('birthday', '')
+        adress = request.POST.get('adress', '')
+        adress_num = request.POST.get('adress_num', '')
+        adress_comp = request.POST.get('adress_comp', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        country = request.POST.get('country', '')
+        zip_code = request.POST.get('zip_code', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        gender = request.POST.get('gender', '')
+        initiations = request.POST.get('initiations', '')
+        initiations_lama = request.POST.get('initiations_lama', '')
+        monastic_ordenation = request.POST.get('monastic_ordenation', '')
+        observations = request.POST.get('observations', '')
+        food_preferency = request.POST.get('food_preferency', '')
+        seat = request.POST.get('seat', '')
+        payment = request.POST.get('payment', '')
+        payment_international = request.POST.get('payment_international', '')
+        deposit_day = request.POST.get('deposit_day', '')
+        deposit_name = request.POST.get('deposit_name', '')
+        deposit_value = request.POST.get('deposit_value', '')
+        deposit_agency = request.POST.get('deposit_agency', '')
+        deposit_account = request.POST.get('deposit_account', '')
+        deposit_envelop = request.POST.get('deposit_envelop', '')
+        credit_card_name = request.POST.get('credit_card_name', '')
+        paypal_name = request.POST.get('paypal_name', '')
+        event_option = request.POST.get('event_option', '')
+
+        # SET THE PAYMENT_INFO
+        if user_language == 'pt-br':
+            if payment == 'Cartão de crédito':
+                payment_info = 'Nome do titular do cartão: ' + credit_card_name
+            else:
+                payment_info = "Depositado o valor de " + deposit_value + " no dia " + deposit_day + " por " + deposit_name + " com os dados: <br>" + "Número do envelope: " + deposit_envelop + "<br>ou<br>" + "Agência: " +  deposit_agency + " - Conta: " + deposit_account
+        else:
+            payment_info = 'Nome do usuário Paypal: ' + paypal_name
+
+        if form.is_valid():
+            sendMail(
+                    name,
+                    birthday,
+                    adress,
+                    adress_num,
+                    adress_comp,
+                    city,
+                    state,
+                    country,
+                    zip_code,
+                    email,
+                    phone,
+                    gender,
+                    initiations,
+                    initiations_lama,
+                    monastic_ordenation,
+                    observations,
+                    food_preferency,
+                    seat,
+                    payment,
+                    payment_international,
+                    payment_info,
+                    event_option,
+                    )
+            return redirect('pay-success')
+
+        else:
+            sendMailError(
+                    name,
+                    birthday,
+                    adress,
+                    adress_num,
+                    adress_comp,
+                    city,
+                    state,
+                    country,
+                    zip_code,
+                    email,
+                    phone,
+                    gender,
+                    initiations,
+                    initiations_lama,
+                    monastic_ordenation,
+                    observations,
+                    food_preferency,
+                    seat,
+                    payment,
+                    payment_international,
+                    payment_info,
+                    event_option,
+                    )
+            if user_language == 'pt-br':
+                return HttpResponseNotFound('<h1>Dados inválidos no formulário. Iremos avaliar os seus dados e entraremos em contato com você. Certifique-se de ter preenchido corretamento o campo e-mail.</h1>')
+            elif user_language == 'es':
+                return HttpResponseNotFound('<h1>Datos no válidos en el formulario. Nosotros evaluamos sus datos y nos pondremos en contacto con usted. Asegúrese de que ha completado el campo de correo electrónico.</h1>')
+            else:
+                return HttpResponseNotFound('<h1>Invalid form data. We will evaluate it and will contact you. Make sure your email has been correctly filled.</h1>')
+
+    else:
+        if user_language == 'pt-br':
+            form = SpecialGuestForm()
+        elif user_language == 'es':
+            form = SpecialGuestForm()
+        else:
+            form = SpecialGuestFormEn()
+
+
+    return render(request, 'special-guest-form.html',  {
         'form': form,
         'user_language': user_language
     })
