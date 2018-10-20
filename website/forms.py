39bbd17ea98b3750291962302b93af91af2e2a9f
barenco <davidbarenco@gmail.com>
2018-10-20 19:15:04 -0300

    Translate the volunteer form correctly in en an pt-br

--- website/forms.py
+++ website/forms.py
@@ -454,7 +454,7 @@ class VolunteerFormEn(forms.Form):
     joint_problems = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control form_text_area',}))
     health_problems = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control form_text_area',}))
     event_option = forms.ChoiceField(required=True, widget=forms.RadioSelect(attrs={'type':'radio', 'class':'form-check inline checkbox-option'}), choices=(
-        ('Voluntário','Alimentación durante el drubchen con alojamiento en la Casa de Retiro: US$ 146.50'),
+        ('Voluntário','Food during the drubchen and accommodation: US$ 146.50'),
     ))
     payment_international = forms.ChoiceField(required=False, widget=forms.RadioSelect(attrs={'type':'radio', 'class':'form-check inline'}), choices=(
         ('PayPal',''),
